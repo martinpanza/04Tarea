1dda07d6bc568964bfcb8fa52401c6277e6b9287
Martín!!! <martin_panza_p@hotmail.com>
2015-10-20 19:23:02 -0300

    Se implementa ecuación de movimiento+Runge Kutta

--- planeta.py
+++ planeta.py
@@ -1,11 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 18:36:30 2015
+
+@author: splatt
+"""
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from scipy.integrate import ode
+import scipy.integrate as integrate 
+import numpy as np
 
 class Planeta(object):
     '''
     Complete el docstring.
     '''
-
     def __init__(self, condicion_inicial, alpha=0):
         '''
         __init__ es un método especial que se usa para inicializar las
@@ -20,15 +30,17 @@ class Planeta(object):
         self.t_actual = 0.
         self.alpha = alpha
 
-    def ecuacion_de_movimiento(self):
+    def ecuacion_de_movimiento(self,t,s):
         '''
         Implementa la ecuación de movimiento, como sistema de ecuaciónes de
         primer orden.
         '''
+        G=6.674e-11 
+        M=19891e+17
         x, y, vx, vy = self.y_actual
-        # fx = ...
-        # fy = ...
-        return [vx, vy, fx, fy]
+        dvxdt=G*M*x/(((x**2)+(y**2))**(3./2.))
+        dvydt=G*M*y/(((x**2)+(y**2))**(3./2.))
+        return [vx, vy, dvxdt, dvydt]
 
     def avanza_euler(self, dt):
         '''
@@ -36,12 +48,20 @@ class Planeta(object):
         en un intervalo de tiempo dt usando el método de Euler explícito. El
         método no retorna nada, pero re-setea los valores de self.y_actual.
         '''
+        self.ecuacion_de_movimiento
         pass
 
     def avanza_rk4(self, dt):
         '''
         Similar a avanza_euler, pero usando Runge-Kutta 4.
         '''
+        I=self.ecuacion_de_movimiento
+        T=[self.t_actual,dt+self.t_actual]
+        S=integrate.odeint(I, self.y_actual, T)
+        self.y_actual=S[1]
+        self.t_actual=T[1]
+        print self.y_actual
+        print self.t_actual
         pass
 
     def avanza_verlet(self, dt):
@@ -55,5 +75,3 @@ class Planeta(object):
         Calcula la enérgía total del sistema en las condiciones actuales.
         '''
         pass
-
-
--- solucion_usando_euler.py
+++ solucion_usando_euler.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from planeta import Planeta
+import numpy as np
 
 
-condicion_inicial = [10, 0, 0, XXX]
+condicion_inicial = [10, 0, 0, 2]
 
 p = Planeta(condicion_inicial)
+dt=10
 
-'''
-Complete el código a continuación.
-'''
+p.avanza_rk4(dt)
+
